12cff0aacb9d0957fdac8e802894875c03551475
Hyy2001X <1804430051@qq.com>
2020-09-15 10:41:53 +0800

    Update diy-part1.sh

--- Scripts/diy-part1.sh
+++ Scripts/diy-part1.sh
@@ -23,8 +23,7 @@ mv $1 ./package/lean
 sed -i "s/#src-git helloworld/src-git helloworld/g" feeds.conf.default
 ExtraPackages_GIT luci-theme-argon https://github.com/jerrykuku 18.06
 ExtraPackages_SVN luci-app-openclash https://github.com/vernesong/OpenClash/trunk
-ExtraPackages_SVN luci-app-adguardhome https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw
-ExtraPackages_SVN AdGuardHome https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t
+ExtraPackages_SVN luci-app-adguardhome https://github.com/Lienol/openwrt/trunk/package/diy
 ExtraPackages_SVN luci-app-smartdns https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t
 ExtraPackages_SVN smartdns https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t
 [ -d ./Openwrt-AutoUpdate ] && rm -rf ./Openwrt-AutoUpdate
