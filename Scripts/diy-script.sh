97925754b0432f5961ac81d53cc5b9a1471c7b27
Hyy2001X <1804430051@qq.com>
2020-09-20 10:37:09 +0800

    Update diy-script.sh

--- Scripts/diy-script.sh
+++ Scripts/diy-script.sh
@@ -62,6 +62,7 @@ echo "Current Openwrt version: $Lede_Version-`date +%Y%m%d`"
 echo "Current Device: $TARGET_PROFILE"
 sed -i "s?$Lede_Version?$Lede_Version Compiled by $Author [$Compile_Date]?g" $Default_File
 echo "$Lede_Version-`date +%Y%m%d`" > ./package/base-files/files/etc/openwrt_date
+echo "$TARGET_PROFILE" > ./package/base-files/files/etc/openwrt_device
 }
 
 Diy-Part3() {
