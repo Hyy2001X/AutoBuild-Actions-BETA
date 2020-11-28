415dbad427c495fc70a7334b697b8919ee3f55cd
Hyy2001X <1804430051@qq.com>
2020-11-28 21:59:56 +0800

    修复更新 mt76 hostapd 导致的编译出错

--- Scripts/diy-script.sh
+++ Scripts/diy-script.sh
@@ -20,8 +20,8 @@ Replace_File banner package/base-files/files/etc
 # ExtraPackages svn network/services dnsmasq https://github.com/openwrt/openwrt/trunk/package/network/services
 # ExtraPackages svn network/services dropbear https://github.com/openwrt/openwrt/trunk/package/network/services
 # ExtraPackages svn network/services ppp https://github.com/openwrt/openwrt/trunk/package/network/services
-ExtraPackages svn kernel mt76 https://github.com/openwrt/openwrt/trunk/package/kernel
-ExtraPackages svn network/services hostapd https://github.com/openwrt/openwrt/trunk/package/network/services
+# ExtraPackages svn kernel mt76 https://github.com/openwrt/openwrt/trunk/package/kernel
+# ExtraPackages svn network/services hostapd https://github.com/openwrt/openwrt/trunk/package/network/services
 
 ExtraPackages git other luci-app-autoupdate https://github.com/Hyy2001X main
 ExtraPackages git lean luci-theme-argon https://github.com/jerrykuku 18.06
