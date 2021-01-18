9fcea5adabc4796290fb5d10e924faa0c3724bff
Hyy2001X <1804430051@qq.com>
2021-01-18 16:34:12 +0800

    mac80211: 合并最新改动

--- Customize/mac80211.sh
+++ Customize/mac80211.sh
@@ -88,6 +88,12 @@ detect_mac80211() {
 			iw phy "$dev" info | grep -q 'VHT Capabilities' && htmode="VHT80"
 		}
 
+		iw phy "$dev" info | grep -q '\* 5.... MHz \[' && {
+			mode_band="ad"
+			channel=$(iw phy "$dev" info | grep '\* 5.... MHz \[' | grep '(disabled)' -v -m 1 | sed 's/[^[]*\[\|\|\].*//g')
+			iw phy "$dev" info | grep -q 'Capabilities:' && htmode="HT20"
+		}
+
 		[ -n "$htmode" ] && ht_capab="set wireless.radio${devidx}.htmode=$htmode"
 
 		path="$(mac80211_phy_to_path "$dev")"
@@ -105,7 +111,7 @@ detect_mac80211() {
 			${dev_id}
 			${ht_capab}
 			set wireless.radio${devidx}.disabled=0
-			set wireless.radio${devidx}.country=CN
+			set wireless.radio${devidx}.country=US
 			set wireless.default_radio${devidx}=wifi-iface
 			set wireless.default_radio${devidx}.device=radio${devidx}
 			set wireless.default_radio${devidx}.network=lan
