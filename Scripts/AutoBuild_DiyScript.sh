457442205302bcefbcd2d2077655071264e7f658
Hyy2001X <1804430051@qq.com>
2021-02-18 22:42:01 +0800

    Drop package mt76 to default version

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -31,8 +31,8 @@ Diy-Part1() {
 
 	Update_Makefile exfat package/kernel/exfat
 
-	Replace_File Customize/mt76-20210127.mk package/kernel/mt76 Makefile
-	rm -rf package/kernel/mt76/patches
+	# Replace_File Customize/mt76-20210127.mk package/kernel/mt76 Makefile
+	# rm -rf package/kernel/mt76/patches
 
 	# ExtraPackages svn network/services dnsmasq https://github.com/openwrt/openwrt/trunk/package/network/services
 	# ExtraPackages svn network/services dropbear https://github.com/openwrt/openwrt/trunk/package/network/services
