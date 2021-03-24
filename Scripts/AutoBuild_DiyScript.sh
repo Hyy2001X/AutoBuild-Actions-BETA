8c4ec5a1d776445ac725cf3b68c0dc1fde1734be
Hyy2001X <1804430051@qq.com>
2021-03-24 20:37:23 +0800

    修复 filebrowser 仓库错误

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -37,7 +37,7 @@ Diy-Part1() {
 	ExtraPackages git other luci-app-serverchan https://github.com/tty228
 	ExtraPackages svn other luci-app-socat https://github.com/Lienol/openwrt-package/trunk
 	ExtraPackages svn other luci-app-usb3disable https://github.com/immortalwrt/luci/trunk/applications
-	ExtraPackages svn other luci-app-filebrowser https://github.com/immortalwrt/immortalwrt/trunk/package/ctcgfw
+	ExtraPackages svn other luci-app-filebrowser https://github.com/immortalwrt/luci/trunk/applications
 	ExtraPackages svn other filebrowser https://github.com/immortalwrt/immortalwrt/trunk/package/ctcgfw
 	ExtraPackages svn lean luci-app-eqos https://github.com/immortalwrt/immortalwrt/trunk/package/ntlf9t
 	ExtraPackages git other luci-app-bearDropper https://github.com/NateLol
