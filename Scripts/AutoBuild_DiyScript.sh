499aeae0acad5f7cf1364ddb3779fa3153ab7d0e
Hyy2001X <1804430051@qq.com>
2021-04-08 17:07:16 +0800

    删除无用文件

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -15,16 +15,12 @@ Diy_Core() {
 
 Firmware-Diy() {
 	Update_Makefile exfat package/kernel/exfat
-	Replace_File CustomFiles/webadmin.po package/lean/luci-app-webadmin/po/zh-cn
 
 	case ${TARGET_PROFILE} in
 	d-team_newifi-d2)
 		Replace_File CustomFiles/mac80211.sh package/kernel/mac80211/files/lib/wifi
-		Replace_File CustomFiles/system_newifi-d2 package/base-files/files/etc/config system
+		Replace_File CustomFiles/system_d-team_newifi-d2 package/base-files/files/etc/config system
 		Replace_File CustomFiles/Patches/102-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.4
-	;;
-	*)
-		Replace_File CustomFiles/system_common package/base-files/files/etc/config system
 	;;
 	esac
 }
