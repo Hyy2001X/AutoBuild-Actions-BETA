51843a731281fd960958ed5b3a90db0fdc1f4e3c
Hyy2001X <1804430051@qq.com>
2021-06-06 13:53:18 +0800

    移除开关 Upload_VM_Firmware(暂时)

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# https://github.com/Hyy2001X/AutoBuild-Actions
-# AutoBuild Module by Hyy2001
+# AutoBuild Module by Hyy2001 <https://github.com/Hyy2001X/AutoBuild-Actions>
 # AutoBuild DiyScript
 
 Diy_Core() {
@@ -14,19 +13,13 @@ Diy_Core() {
 	INCLUDE_DRM_I915=true
 	INCLUDE_Theme_Argon=true
 	INCLUDE_Obsolete_PKG_Compatible=false
-
-	Upload_VM_Firmware=false
 }
 
 Firmware-Diy() {
-	TIME "Starting run Firmware-Diy commands..."
-	AddPackage git other luci-app-adguardhome Hyy2001X
-
-	case ${TARGET_PROFILE} in
+	case "${TARGET_PROFILE}" in
 	d-team_newifi-d2)
 		Replace_File CustomFiles/mac80211.sh package/kernel/mac80211/files/lib/wifi
 		Replace_File CustomFiles/system_d-team_newifi-d2 package/base-files/files/etc/config system
 		# Replace_File CustomFiles/Patches/102-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.4
-	;;
 	esac
 }
