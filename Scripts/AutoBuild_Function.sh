8969b918b4a67d237ecd80621f219c1b4b539c9c
Hyy2001X <1804430051@qq.com>
2021-06-25 21:49:38 +0800

    移除无用内容

--- Scripts/AutoBuild_Function.sh
+++ Scripts/AutoBuild_Function.sh
@@ -128,7 +128,6 @@ Openwrt_Branch=${Openwrt_Branch}
 AutoBuild_Firmware=${AutoBuild_Firmware}
 Openwrt_Repo_Name=${Openwrt_Repo_Name}
 Egrep_Firmware=${Egrep_Firmware}
-FW_SAVE_PATH=/tmp/Downloads
 EOF
 	cat >> VARIABLE_FILE_Sec <<EOF
 INCLUDE_Obsolete_PKG_Compatible=${INCLUDE_Obsolete_PKG_Compatible}
