318e52d3b481fc7e5c023ac5dc2a529b9a43b4f4
Hyy2001X <1804430051@qq.com>
2022-01-05 18:02:00 +0800

    Add patch

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -55,6 +55,7 @@ Firmware_Diy() {
 		# AddPackage git other OpenAppFilter destan19 master
 		# AddPackage svn other luci-app-ddnsto linkease/nas-packages/trunk/luci
 		# AddPackage svn other ddnsto linkease/nas-packages/trunk/network/services
+		cat ${CustomFiles}/Patches/revert_remove-alterId-config.patch | patch -p1 -d ${Home}/package/other
 
 		case "${TARGET_PROFILE}" in
 		d-team_newifi-d2)
