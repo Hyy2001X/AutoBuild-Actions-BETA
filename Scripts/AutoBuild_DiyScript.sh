9f82df8b7c733ee1625af4febc8a1fe35f7f93cb
Hyy2001X <1804430051@qq.com>
2022-01-08 11:54:16 +0800

    添加 Patch: fix_ntfs3_antfs_conflict.patch

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -56,6 +56,7 @@ Firmware_Diy() {
 		# AddPackage svn other luci-app-ddnsto linkease/nas-packages/trunk/luci
 		# AddPackage svn other ddnsto linkease/nas-packages/trunk/network/services
 		patch < ${CustomFiles}/Patches/revert_remove-alterId-config.patch -p0 -d ${Home}/package/other
+		patch < ${CustomFiles}/Patches/fix_ntfs3_antfs_conflict.patch -p0 -d ${FEEDS_PKG}
 
 		case "${TARGET_PROFILE}" in
 		d-team_newifi-d2)
