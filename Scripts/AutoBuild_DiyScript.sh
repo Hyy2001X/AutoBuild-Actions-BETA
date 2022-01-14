1ab02e49b4ba2b66021c50e489fd4f7994c991e4
Hyy2001X <1804430051@qq.com>
2022-01-14 12:04:50 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -54,8 +54,8 @@ Firmware_Diy() {
 		# AddPackage git other OpenAppFilter destan19 master
 		# AddPackage svn other luci-app-ddnsto linkease/nas-packages/trunk/luci
 		# AddPackage svn other ddnsto linkease/nas-packages/trunk/network/services
-		patch < ${CustomFiles}/Patches/revert_remove-alterId-config.patch -p0 -d ${Home}
-		patch < ${CustomFiles}/Patches/fix_ntfs3_antfs_conflict.patch -p0 -d ${Home}
+		patch < ${CustomFiles}/Patches/revert_remove-alterId-config.patch -p1 -d ${Home}
+		patch < ${CustomFiles}/Patches/fix_ntfs3_antfs_conflict.patch -p1 -d ${Home}
 
 		case "${TARGET_PROFILE}" in
 		d-team_newifi-d2)
