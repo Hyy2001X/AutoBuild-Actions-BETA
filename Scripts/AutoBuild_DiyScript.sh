9ce2546d5ecaf66dfe7aa874544271afd5b7cffa
Hyy2001X <1804430051@qq.com>
2022-01-15 14:42:04 +0800

    Sync

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -56,10 +56,11 @@ Firmware_Diy() {
 		# AddPackage svn other ddnsto linkease/nas-packages/trunk/network/services
 		patch < ${CustomFiles}/Patches/revert_remove-alterId-config.patch -p1 -d ${Home}
 		patch < ${CustomFiles}/Patches/fix_ntfs3_antfs_conflict.patch -p1 -d ${Home}
+		patch < ${CustomFiles}/Patches/fix_aria2_autocreate_path.patch -p1 -d ${Home}
 
 		case "${TARGET_PROFILE}" in
 		d-team_newifi-d2)
-			patch -i ${CustomFiles}/${TARGET_PROFILE}_mac80211.patch package/kernel/mac80211/files/lib/wifi/mac80211.sh
+			patch < ${CustomFiles}/${TARGET_PROFILE}_mac80211.patch -p1 -d ${Home}
 			Copy ${CustomFiles}/${TARGET_PROFILE}_system ${BASE_FILES}/etc/config system
 			sed -i "/DEVICE_COMPAT_VERSION := 1.1/d" target/linux/ramips/image/mt7621.mk
 		;;
