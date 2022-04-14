01a483ea84dcbb1ca1443a2446dc75f810abe6ca
Hyy2001X <1804430051@qq.com>
2022-04-14 12:58:57 +0800

    Tests

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -48,13 +48,9 @@ Firmware_Diy() {
 		AddPackage git lean luci-app-argon-config jerrykuku master
 		AddPackage git other AutoBuild-Packages Hyy2001X master
 		AddPackage svn other luci-app-smartdns kenzok8/openwrt-packages/trunk
-		AddPackage svn other luci-app-socat Lienol/openwrt-package/trunk
 		AddPackage svn other luci-app-eqos kenzok8/openwrt-packages/trunk
 		AddPackage git other OpenClash vernesong master
 		AddPackage git other luci-app-ikoolproxy iwrt main
-		# AddPackage git other OpenAppFilter destan19 master
-		# AddPackage svn other luci-app-ddnsto linkease/nas-packages/trunk/luci
-		# AddPackage svn other ddnsto linkease/nas-packages/trunk/network/services
 		AddPackage git other helloworld fw876 master
 		sed -i 's/143/143,8080,8443/' $(PKG_Finder d package luci-app-ssr-plus)/root/etc/init.d/shadowsocksr
 		patch < ${CustomFiles}/Patches/revert_remove-alterId-config.patch -p1 -d ${WORK}
