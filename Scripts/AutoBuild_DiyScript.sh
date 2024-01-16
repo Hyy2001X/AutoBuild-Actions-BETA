619610ae6f55dffcecf0885b9c525a3148e9f04f
Hyy2001X <1804430051@qq.com>
2024-01-16 16:51:07 +0800

    Update

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -4,25 +4,41 @@
 
 Firmware_Diy_Core() {
 
+	# 请在该函数内按需修改变量设置, 使用 case 语句控制不同预设变量的设置
+	
+	# 可用预设变量
+	# ${OP_AUTHOR}			OpenWrt 源码作者
+	# ${OP_REPO}				OpenWrt 仓库名称
+	# ${OP_BRANCH}			OpenWrt 源码分支
+	# ${CONFIG_FILE}			配置文件
+	
 	Author=AUTO
 	# 作者名称, AUTO: [自动识别]
+	
 	Author_URL=AUTO
 	# 自定义作者网站或域名, AUTO: [自动识别]
+	
 	Default_Flag=AUTO
 	# 固件标签 (名称后缀), 适用不同配置文件, AUTO: [自动识别]
+	
 	Default_IP="192.168.1.1"
 	# 固件 IP 地址
+	
 	Default_Title="Powered by AutoBuild-Actions"
 	# 固件终端首页显示的额外信息
 	
 	Short_Fw_Date=true
 	# 简短的固件日期, true: [20210601]; false: [202106012359]
+	
 	x86_Full_Images=false
 	# 额外上传已检测到的 x86 虚拟磁盘镜像, true: [上传]; false: [不上传]
+	
 	Fw_MFormat=AUTO
 	# 自定义固件格式, AUTO: [自动识别]
+	
 	Regex_Skip="packages|buildinfo|sha256sums|manifest|kernel|rootfs|factory|itb|profile|ext4|json"
 	# 输出固件时丢弃包含该内容的固件/文件
+	
 	AutoBuild_Features=true
 	# 添加 AutoBuild 固件特性, true: [开启]; false: [关闭]
 	
