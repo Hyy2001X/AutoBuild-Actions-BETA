b61f7b2998332041edf06d4a717e828a23ab4aed
Hyy2001X <1804430051@qq.com>
2024-04-05 21:31:01 +0800

    x86_64: add immortalwrt 23.05 version

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -145,26 +145,6 @@ EOF
 			rm -r ${WORK}/package/passwall/openwrt-passwall-packages/xray-plugin
 			# rm -rf packages/lean/autocore
 			# AddPackage lean Hyy2001X autocore-modify master
-
-			singbox_version="1.8.10"
-			hysteria_version="2.4.0"
-			wstunnel_version="9.2.5"
-			wget --quiet --no-check-certificate -P /tmp \
-				https://github.com/SagerNet/sing-box/releases/download/v${singbox_version}/sing-box-${singbox_version}-linux-amd64.tar.gz
-			wget --quiet --no-check-certificate -P /tmp \
-				https://github.com/apernet/hysteria/releases/download/app%2Fv${hysteria_version}/hysteria-linux-amd64
-			wget --quiet --no-check-certificate -P /tmp \
-				https://github.com/erebe/wstunnel/releases/download/v${wstunnel_version}/wstunnel_${wstunnel_version}_linux_amd64.tar.gz
-			tar -xvzf /tmp/sing-box-${singbox_version}-linux-amd64.tar.gz -C /tmp
-			tar -xvzf /tmp/wstunnel_${wstunnel_version}_linux_amd64.tar.gz -C /tmp
-			Copy /tmp/sing-box-${singbox_version}-linux-amd64/sing-box ${BASE_FILES}/usr/bin
-			Copy /tmp/wstunnel ${BASE_FILES}/usr/bin
-			Copy /tmp/hysteria-linux-amd64 ${BASE_FILES}/usr/bin hysteria
-
-			chmod 777 ${BASE_FILES}/usr/bin/sing-box ${BASE_FILES}/usr/bin/hysteria
-
-			# ReleaseDL https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases/latest geosite.dat ${BASE_FILES}/usr/v2ray
-			# ReleaseDL https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases/latest geoip.dat ${BASE_FILES}/usr/v2ray
 		;;
 		xiaomi_redmi-router-ax6s)
 			AddPackage passwall-depends xiaorouji openwrt-passwall-packages main
@@ -176,19 +156,21 @@ EOF
 		case "${TARGET_PROFILE}" in
 		x86_64)
 			Copy ${CustomFiles}/Depends/cpuset ${BASE_FILES}/bin
-			# sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${FEEDS_PKG}/ttyd/files/ttyd.config
 			sed -i -- 's:/bin/ash:'/bin/bash':g' ${BASE_FILES}/etc/passwd
-			AddPackage passwall xiaorouji openwrt-passwall2 main
-			AddPackage passwall xiaorouji openwrt-passwall main
-			rm -r ${FEEDS_LUCI}/luci-app-passwall
-			AddPackage other fw876 helloworld main
-			rm -r ${WORK}/package/other/helloworld/mosdns
-			rm -r ${FEEDS_PKG}/mosdns
-			AddPackage other sbwml luci-app-mosdns v5
-			AddPackage other vernesong OpenClash dev
-			ClashDL amd64 dev
-			ClashDL amd64 tun
-			ClashDL amd64 meta
+			Copy ${CustomFiles}/curl ${FEEDS_PKG}
+			
+			case "${CONFIG_FILE}" in
+			x86_64-Next)
+				# sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${FEEDS_PKG}/ttyd/files/ttyd.config
+				AddPackage passwall xiaorouji openwrt-passwall2 main
+				AddPackage passwall xiaorouji openwrt-passwall main
+				rm -r ${FEEDS_LUCI}/luci-app-passwall
+				rm -r ${FEEDS_PKG}/mosdns
+				rm -r ${FEEDS_PKG}/xray-core
+				rm -r ${FEEDS_PKG}/xray-plugin
+				AddPackage other sbwml luci-app-mosdns v5
+			;;
+			esac
 		;;
 		esac
 	;;
@@ -211,6 +193,29 @@ EOF
 			patch < ${CustomFiles}/mt7981/0001-Add-iptables-socket.patch -p1 -d ${WORK}
 		;;
 		esac
+	;;
+	esac
+	case "${TARGET_PROFILE}" in
+	x86_64)
+		singbox_version="1.8.10"
+		hysteria_version="2.4.0"
+		wstunnel_version="9.2.5"
+		wget --quiet --no-check-certificate -P /tmp \
+			https://github.com/SagerNet/sing-box/releases/download/v${singbox_version}/sing-box-${singbox_version}-linux-amd64.tar.gz
+		wget --quiet --no-check-certificate -P /tmp \
+			https://github.com/apernet/hysteria/releases/download/app%2Fv${hysteria_version}/hysteria-linux-amd64
+		wget --quiet --no-check-certificate -P /tmp \
+			https://github.com/erebe/wstunnel/releases/download/v${wstunnel_version}/wstunnel_${wstunnel_version}_linux_amd64.tar.gz
+		tar -xvzf /tmp/sing-box-${singbox_version}-linux-amd64.tar.gz -C /tmp
+		tar -xvzf /tmp/wstunnel_${wstunnel_version}_linux_amd64.tar.gz -C /tmp
+		Copy /tmp/sing-box-${singbox_version}-linux-amd64/sing-box ${BASE_FILES}/usr/bin
+		Copy /tmp/wstunnel ${BASE_FILES}/usr/bin
+		Copy /tmp/hysteria-linux-amd64 ${BASE_FILES}/usr/bin hysteria
+
+		chmod 777 ${BASE_FILES}/usr/bin/sing-box ${BASE_FILES}/usr/bin/hysteria ${BASE_FILES}/usr/bin/wstunnel
+
+		# ReleaseDL https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases/latest geosite.dat ${BASE_FILES}/usr/v2ray
+		# ReleaseDL https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases/latest geoip.dat ${BASE_FILES}/usr/v2ray
 	;;
 	esac
 }
