46ea0843e41a2ce951d45919326c35c3892f022d
Hyy2001X <1804430051@qq.com>
2024-06-13 12:24:27 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -208,9 +208,9 @@ EOF
 		Copy ${CustomFiles}/Depends/cpuset ${BASE_FILES}/bin
 		ReleaseDL https://api.github.com/repos/nxtrace/NTrace-core/releases/latest nexttrace_linux_amd64 ${BASE_FILES}/bin nexttrace
 
-		singbox_version="1.10.0-alpha.7"
-		hysteria_version="2.4.3"
-		wstunnel_version="9.6.1"
+		singbox_version="1.10.0-alpha.11"
+		hysteria_version="2.4.5"
+		wstunnel_version="9.7.0"
 		cloudflared_version="2024.6.0"
 		wget --quiet --no-check-certificate -P /tmp \
 			https://github.com/SagerNet/sing-box/releases/download/v${singbox_version}/sing-box-${singbox_version}-linux-amd64.tar.gz
