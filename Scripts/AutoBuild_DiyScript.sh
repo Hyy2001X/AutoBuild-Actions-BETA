c9ef05d3be0b5eae04d6047dc17b5c77f5761448
Hyy2001X <1804430051@qq.com>
2024-11-07 21:40:53 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -96,8 +96,8 @@ EOF
 		# sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${FEEDS_PKG}/ttyd/files/ttyd.config
 		# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 		# sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/argon-mod"' $(PKG_Finder d package default-settings)/files/zzz-default-settings
-		sed -i "s?openwrt-23.05?master?g" ${FEEDS_CONF}
-
+		#sed -i "s?openwrt-23.05?master?g" ${FEEDS_CONF}
+		git reset --hard 1627fd2c745e496134834a8fb8145ba0aa458ae9
 		rm -r ${FEEDS_LUCI}/luci-theme-argon*
 		AddPackage other vernesong OpenClash dev
 		AddPackage other jerrykuku luci-app-argon-config master
