473a84b85c4cd6024f05c8f67ebb6983b6ea04f5
Hyy2001X <1804430051@qq.com>
2025-02-22 16:14:40 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -165,6 +165,8 @@ EOF
 				rm -r ${FEEDS_PKG}/socat/files
 				Copy ${CustomFiles}/speedtest ${BASE_FILES}/usr/bin
 				chmod +x ${BASE_FILES}/usr/bin/speedtest
+				
+				sed -i '/PKG_FIXUP/d' ${WORK}/feeds/packages/libs/libffi/Makefile
 			;;
 			esac
 		;;
